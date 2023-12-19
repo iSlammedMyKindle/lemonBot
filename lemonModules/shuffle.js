d1cef44ac097626a48f7a0b6484ef83da58270e2
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-12-19 17:46:33 -0300

    Implement Fisher-Yates shuffle in shuf command

--- lemonModules/shuffle.js
+++ lemonModules/shuffle.js
@@ -1,4 +1,10 @@
-//Simple shuffling pattern. Note this list isn't copied but rearranged, so your original array will be affected.
-var shuffle = (list = [])=>list.sort((e,f)=>(Math.floor(Math.random()*2) ? 1:-1));
+// Shuffle items using unbiased Fisher-Yates shuffle algorithm (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+const shuffle = (list = []) => {
+    for (let i = list.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [list[i], list[j]] = [list[j], list[i]]
+    }
+    return list;
+};
 
 if(typeof module == 'object' && module.exports) module.exports = shuffle;
